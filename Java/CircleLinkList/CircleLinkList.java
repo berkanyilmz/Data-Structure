56baa79a2e65aa9ee4fe00cc466e191c45d6c8d7
Berkan YILMAZ <55620672+berkanyilmz@users.noreply.github.com>
2022-05-03 13:46:21 +0300

    Update CircleLinkList.java

--- Java/CircleLinkList/CircleLinkList.java
+++ Java/CircleLinkList/CircleLinkList.java
@@ -24,7 +24,7 @@ public class CircleLinkList {
         return false;
     }
 
-     void addHead(Node node) {
+     void add(Node node) {
         if (isEmpty()) {
             head = node;
             head.next = node;
@@ -40,48 +40,7 @@ public class CircleLinkList {
         System.out.println("Adding is successful");
     }
 
-    void addTail(Node node) {
-        if (isEmpty()) {
-            head = node;
-            head.next = node;
-            head.back = node;
-        } else {
-            node.back = head.back;
-            head.back.next = node;
-            node.next = head;
-            head.back = node;
-        }
-        size++;
-        System.out.println("Adding is successful");
-    }
-
-    void add(Node node, int after) {
-        if (isEmpty()) {
-            System.out.println("List is empty");
-        }
-        else if (isThere(after)) {
-            if (node.number == head.back.number) {
-                addTail(node);
-            }
-            else {
-                Node temp = head;
-                while (temp.number != after)
-                    temp = temp.next;
-
-                node.back = temp;
-                node.next = temp.next;
-                node.next.back = node;
-                node.back.next = node;
-            }
-            size++;
-            System.out.println("Adding is successful");
-        }
-        else {
-            System.out.println("Value is not in list");
-        }
-    }
-
-    Node delHead() {
+    Node del() {
         if (isEmpty()) {
             System.out.println("List is empty");
             return null;
@@ -93,53 +52,6 @@ public class CircleLinkList {
         size--;
         System.out.println("Deleting is successful");
         return deleted;
-    }
-
-    Node delTail() {
-        if (isEmpty()) {
-            System.out.println("List is empty");
-            return null;
-        }
-        Node deleted = head.back;
-        head.back.back.next = head;
-        head.back = head.back.back;
-        size--;
-        System.out.println("Deleting is successful");
-        return deleted;
-    }
-
-    Node del(int value) {
-        if (isEmpty()) {
-            System.out.println("List is empty");
-            return null;
-        }
-        else if(isThere(value)) {
-            if (head.number == value) {
-                size--;
-                System.out.println("Deleting is successful");
-                return delHead();
-            }
-            else if (head.back.number == value) {
-                size--;
-                System.out.println("Deleting is successful");
-                return delTail();
-            }
-
-            Node temp = head;
-            while (temp.number != value)
-                temp = temp.next;
-
-            Node deleted = temp;
-            temp.back.next = temp.next;
-            temp.next.back = temp.back;
-            size--;
-            System.out.println("Deleting is successful");
-            return deleted;
-        }
-        else {
-            System.out.println("Value is not in list");
-            return null;
-        }
     }
 
     void showForward() {
