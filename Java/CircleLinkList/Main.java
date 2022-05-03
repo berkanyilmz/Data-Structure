4a8858c3e988e8ab696dee09c3b75edeba961935
Berkan YILMAZ <55620672+berkanyilmz@users.noreply.github.com>
2022-05-03 13:54:18 +0300

    Update Main.java

--- Java/CircleLinkList/Main.java
+++ Java/CircleLinkList/Main.java
@@ -5,32 +5,32 @@ public class Main {
     public static void main(String[] args) {
         CircleLinkList list = new CircleLinkList();
 
-        list.addHead(new Node(1));
-        list.addTail(new Node(2));
-        list.addTail(new Node(3));
-        list.addTail(new Node(4));
-        list.addTail(new Node(5));
-        list.addHead(new Node(0));
-        list.add(new Node(8), 3);
+        list.add(new Node(1));
+        list.add(new Node(2));
+        list.add(new Node(3));
+        list.add(new Node(4));
+        list.add(new Node(5));
+        list.add(new Node(0));
+        list.add(new Node(8));
 
         list.showForward();
         System.out.println();
         list.showBackward();
         System.out.println();
         System.out.println("Deleting head");
-        list.delHead();
+        list.del();
         list.showForward();
         System.out.println();
         list.showBackward();
 
         System.out.println("Deleting tail");
-        list.delTail();
+        list.del();
         list.showBackward();
         System.out.println();
         list.showForward();
 
         System.out.println("Deleting any node");
-        list.del(3);
+        list.del();
         list.showBackward();
         System.out.println();
         list.showForward();
