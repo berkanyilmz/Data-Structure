89abd96148abbd6b71812e8d82eb07b8aef2179e
Berkan YILMAZ <55620672+berkanyilmz@users.noreply.github.com>
2022-06-30 16:11:33 +0300

    Delete Python/Sort directory

--- Python/Sort/PostfixExpression/node.py
+++
@@ -1,8 +0,0 @@
-from operators import Operators
-
-class Node:
-
-    def __init__(self, opr:Operators='', value = -1):
-        self.value = value
-        self.operator = opr
-        self.down:Node = None
